0a32e01944d574644fd686e2e179c35e860f4374
metetrkn <metetrkn52@gmail.com>
2025-04-02 15:19:44 +0200

    Pipeline completed

--- src/main/java/org/example/javaweb/utils/DataSeeder.java
+++ src/main/java/org/example/javaweb/utils/DataSeeder.java
@@ -1,16 +1,19 @@
 package org.example.javaweb.utils;
 
 import com.github.javafaker.Faker;
+import jakarta.annotation.Resource;
 import org.example.javaweb.model.Dog;
 import org.example.javaweb.model.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Enumeration;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -23,38 +26,45 @@ public class DataSeeder {
 
     Faker faker = new Faker();
 
-    public void Seed() throws URISyntaxException {
-        if(dogRepository.count() > 0 ){
+    public void Seed() throws URISyntaxException, IOException {
+        if (dogRepository.count() > 0) {
             return;
         }
-        for(int i =0; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             dogRepository.save(RandomDog());
         }
     }
 
-    private Dog RandomDog() throws URISyntaxException {
+    private Dog RandomDog() throws URISyntaxException, IOException {
         Dog dog = new Dog();
         dog.setAge(faker.dog().age());
         dog.setName(faker.dog().name());
         dog.setBreed(faker.dog().breed());
         dog.setGender(faker.dog().gender());
-        dog.setPrice(faker.random().nextInt(4,20) * 1000);
+        dog.setPrice(faker.random().nextInt(4, 20) * 1000);
         dog.setSize(faker.dog().size());
         dog.setImage("/images/dogs/" + getRandomImage());
         return dog;
     }
 
-    private String getRandomImage() throws URISyntaxException {
-        //Get all files in dir
-        URL resource = getClass().getClassLoader().getResource("static/images/dogs");
-        Path dir = Paths.get(resource.toURI());
+    private String getRandomImage() throws URISyntaxException, IOException {
+        int []arr={0,1,2,3,8,9,24,25,30,34,36,38,40,42,50,60,61,63,66,84,85,88,91,92,94,197,110,111,112,116,117,135,144,146,152};
+        int rnd = new Random().nextInt(arr.length);
+        String s = String.valueOf(arr[rnd]);
+        return "dog." + s + ".jpg";
+//        Path dir = Paths.get(resource.toURI());
+//
+//        Set<String> allFiles =  Stream.of(new File(String.valueOf(dir)).listFiles())
+//                .filter(file -> !file.isDirectory())
+//                .map(File::getName)
+//                .collect(Collectors.toSet());
+//
+//        return getByRandomClass(allFiles);
+    }
 
-        Set<String> allFiles =  Stream.of(new File(String.valueOf(dir)).listFiles())
-                .filter(file -> !file.isDirectory())
-                .map(File::getName)
-                .collect(Collectors.toSet());
-
-        return getByRandomClass(allFiles);
+    public static String getRandom(String[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
     }
 
     public static <T> T getByRandomClass(Set<T> set) {
